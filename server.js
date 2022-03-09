027e3a5e18081b6efe75597ba3ff4b8aab4f926c
SaruMan64 <yete.abuna@gmail.com>
2022-03-09 16:07:12 -0300

    Criando rota que retorna número aleatório com delay

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const port = 4444;
+const port = 4000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/randomnumber", (req, res) => {
+  function randomNumber(min, max) {
+    return (Math.random() * (max - min) + min).toFixed(3);
+  }
+
+  const number = randomNumber(0, 5);
+  setTimeout(() => {
+    console.log(number)
+    res.send(`${number}`);
+  }, number * 1000);
 
 });
 
+/* 
 app.post("/", (req, res) => {
   
 });
@@ -21,6 +31,7 @@ app.put("/:id", (req, res) => {
 app.delete("/:id", (req, res) => {
   
 });
+ */
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
